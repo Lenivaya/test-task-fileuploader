433ecf3d6e8209f5a49a394b2cb787414785e06c
Lenivaya <danylo.osipchuk@proton.me>
2025-05-01 18:41:35 +0300

    chore: update Prisma client initialization to use default client
    - Commented out the PrismaPg adapter and environment variable imports due to issues with the latest Prisma update.
    - Retained the default PrismaClient initialization for continued functionality while addressing the adapter's compatibility problems.
    - Added references to relevant GitHub issues for context on the changes made.

--- packages/trpc/src/prisma/index.ts
+++ packages/trpc/src/prisma/index.ts
@@ -1,7 +1,11 @@
-import { PrismaPg } from '@prisma/adapter-pg'
-import { env } from '../env'
+// import { PrismaPg } from '@prisma/adapter-pg'
+// import { env } from '../env'
 import { PrismaClient } from '../../generated/prisma'
 
+// seems like broken since last prisma update, use default client for now
+// [1]: https://github.com/prisma/prisma/issues/27050
+// [2]: https://github.com/prisma/prisma/issues/27042
+//
 // const adapter = new PrismaPg({ connectionString: env.DATABASE_URL })
 // const prisma = new PrismaClient({ adapter })
 const prisma = new PrismaClient({})
